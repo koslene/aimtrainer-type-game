18959a53f6da328f7144c4181a97772a2ce2c846
koslene <artiszvirbulis3@gmail.com>
2025-10-05 15:45:32 +0300

    asasd

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Store scores in memory for now
+scores = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit_score', methods=['POST'])
+def submit_score():
+    data = request.get_json()
+    score = data.get('score', 0)
+    scores.append(score)
+    return jsonify({'message': 'Score received!', 'scores': scores})
+
+if __name__ == '__main__':
+    app.run(debug=True)
